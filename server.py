5e5c1ddaed526787120d1b4516718859df99efd6
Darkrai-Design <aneeshlabh@gmail.com>
2026-05-26 15:19:23 -0400

    Completed final project

--- server.py
+++ server.py
@@ -1,54 +1,59 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+"""
+Emotion Detection Flask server.
+"""
+
+from flask import Flask, render_template, request
 
 from EmotionDetection import emotion_detector
 
-app = Flask(
-    "Emotion Detector"
-)
+app = Flask("Emotion Detector")
 
 
 @app.route("/")
 def render_index_page():
     """
-    Render home page.
+    Render application homepage.
     """
-    return render_template(
-        'index.html'
-    )
+    return render_template("index.html")
 
 
 @app.route("/emotionDetector")
 def emotion_detection():
+    """
+    Process emotion analysis request.
+    """
 
     text_to_analyze = request.args.get(
-        'textToAnalyze'
+        "textToAnalyze"
     )
 
     response = emotion_detector(
         text_to_analyze
     )
 
-    formatted_response = (
-        "For the given statement, "
-        "the system response is "
+    if response["dominant_emotion"] is None:
+
+        return (
+            "Invalid text! "
+            "Please try again!"
+        )
+
+    return (
+        f"For the given statement, "
+        f"the system response is "
         f"'anger': {response['anger']}, "
         f"'disgust': {response['disgust']}, "
         f"'fear': {response['fear']}, "
-        f"'joy': {response['joy']} "
-        f"and 'sadness': "
-        f"{response['sadness']}. "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
         f"The dominant emotion is "
         f"{response['dominant_emotion']}."
     )
 
-    return formatted_response
-
 
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=5000
     )
-
